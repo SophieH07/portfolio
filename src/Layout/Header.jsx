3ef2100902c82d8411997a59ca01f7739228968c
Sophie <szaniszlozs7@gmail.com>
2023-07-13 14:50:36 +0200

    added tailwind css

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
   return (
     <div>
-      <h2>Hello on my website!</h2>
+      <h2 className="text-3xl font-bold">Hello on my website!</h2>
       <h3>I'm Sophie, a Software Developer from Hungary</h3>
     </div>
   );
