a4d58def3d4440fd695eb285387dcdfce45117c0
Sophie <szaniszlozs7@gmail.com>
2023-10-03 00:23:37 +0200

    change footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,7 +1,17 @@
 const Footer = () => {
   return (
-    <div className="w-full bg-slate-700 bg-opacity-30 uppercase font-medium text-center text-xl py-2">
-      Made with: Vite, React, Tailwind CSS
+    <div className="w-full flex justify-center bg-slate-700 bg-opacity-30 uppercase font-medium text-slate-950 py-2">
+      <div className="text-xl pr-4">Made with: Vite, React, Tailwind CSS</div>{" "}
+      <div className="text-lg">
+        You can find the code of this portfolio{" "}
+        <a
+          href="https://github.com/SophieH07/portfolio"
+          target="_blank"
+          className="hover:text-white"
+        >
+          here
+        </a>{" "}
+      </div>
     </div>
   );
 };
